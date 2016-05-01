a48a18c0a1d2b5b81d2f4e334e04ea1b367543c6
Alex Jordan <alex@strugee.net>
2016-04-30 22:54:11 -0700

    Add 'css' Gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,19 @@
 var gulp = require('gulp');
 var jade = require('gulp-jade');
+var stylus = require('gulp-stylus');
+var rename = require('gulp-rename');
 
 gulp.task('html', function() {
 	return gulp.src('src/*.jade')
 		   .pipe(jade({ data: require('./data/nodecompat-data.json') }))
 	           .pipe(gulp.dest('dist'));
 });
+
+gulp.task('css', function() {
+	return gulp.src('src/styles/*.styl')
+	           .pipe(stylus())
+	           .pipe(rename({extname: '.css'}))
+	           .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('build', ['html', 'css']);
